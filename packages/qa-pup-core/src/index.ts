54fc666955c0135bddaf62a4de7dd27dfc972672
cloudydaiyz <kyland03.biz@gmail.com>
2024-09-10 16:35:36 -0500

    init repo

---
+++ packages/qa-pup-core/src/index.ts
@@ -0,0 +1,25 @@
+function readDashboardInfo() {
+
+}
+
+function readLatestTestInfo() {
+
+}
+
+function refreshInfo() {
+
+}
+
+function triggerManualRun() {
+
+}
+
+// remove tests that are over a week old
+function testCleanup() {
+
+}
+
+// refresh the number of manual runs
+function refreshManualRuns() {
+
+}
---
+++ packages/qa-pup-serverless/src/index.ts
@@ -0,0 +1,3 @@
+function sendTestCompletionEmail(emailList: string[]) {
+
+}
