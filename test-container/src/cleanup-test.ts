00c61e1e762e461649b31984a7a4447261e3a355
cloudydaiyz <kyland03.biz@gmail.com>
2024-09-11 14:25:22 -0500

    test-container sample

--- test-container/src/cleanup-test.ts
+++ test-container/src/cleanup-test.ts
@@ -1,12 +1,44 @@
 // export assets to test-artifacts bucket
 
+import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import assert from "assert";
+import fs from "fs";
 
-const artifactsBucket = process.env.TEST_ARTIFACTS_BUCKET;
+// const artifactsBucket = process.env.TEST_ARTIFACTS_BUCKET;
+const client = new S3Client({ region: "us-east-1" });
 
-async function cleanup() {
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_s3_code_examples.html
+async function cleanupSample() {
     // assert(artifactsBucket, 'TEST_ARTIFACTS_BUCKET is required');
+    const data = fs.readFileSync('./write-example/Auto-Scaling-Lab.yaml');
+    console.log(data.toString());
+
     console.log('cleaning');
+    const command = new PutObjectCommand({
+        Bucket: "cloudydaze-output-bucket",
+        Key: "EC2-Auto-Scaling-Lab.yaml",
+        Body: data
+    });
+    const response = await client.send(command);
+    assert(response.ETag, "No response body");
+    console.log(await response.ETag);
+
+    const command2 = new GetObjectCommand({
+        Bucket: "cloudydaze-output-bucket",
+        Key: "EC2-Auto-Scaling-Lab.yaml",
+    });
+    const response2 = await client.send(command2);
+    assert(response2.Body, "No response body");
+    const response2String = await response2.Body.transformToString();
+    console.log(response2String);
+    
+    // Assert that the two are the same
+    console.log(data.toString() == response2String);
 }
 
-cleanup();
+async function cleanup() {
+    
+}
+
+cleanupSample();
+// cleanup();
--- test-container/src/prepare-test.ts
+++ test-container/src/prepare-test.ts
@@ -1,12 +1,31 @@
 // import tests from S3 bucket into the ./tests folder
 
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import assert from "assert";
+import fs from "fs";
 
-const codeBucket = process.env.TEST_CODE_BUCKET;
+// const codeBucket = process.env.TEST_CODE_BUCKET;
+const client = new S3Client({ region: "us-east-1" });
 
-async function prepare() {
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_s3_code_examples.html
+async function prepareSample() {
     // assert(codeBucket, 'TEST_CODE_BUCKET is required');
+        
     console.log('preparing');
+    const command = new GetObjectCommand({
+        Bucket: "cf-templates-jy2ivedl9rti-us-east-1",
+        Key: "2024-06-10T203259.224Z9fz-EC2-Auto-Scaling-Lab.yaml"
+    });
+    const response = await client.send(command);
+    assert(response.Body, "No response body");
+    // console.log(await response.Body.transformToString());
+    fs.mkdirSync('./write-example', { recursive: true });
+    fs.writeFileSync('./write-example/Auto-Scaling-Lab.yaml', await response.Body.transformToByteArray(), {flag: 'w'});
 }
 
-prepare();
+async function prepare() {
+    
+}
+
+prepareSample();
+// prepare();
