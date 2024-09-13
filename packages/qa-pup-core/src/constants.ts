4912ffa9aef527dddff562d4df8370a036ae4809
cloudydaiyz <kyland03.biz@gmail.com>
2024-09-13 14:46:29 -0500

    project reorganized

--- packages/qa-pup-core/src/constants.ts
+++ packages/qa-pup-core/src/constants.ts
@@ -1,7 +1,7 @@
-export const MAX_DAILY_MANUAL_TESTS = 3;
-
 export const FULL_HOUR = 60 * 60 * 1000;
 export const FULL_DAY = 24 * FULL_HOUR;
 export const TEST_LIFETIME = 7 * FULL_DAY;
+
+export const MAX_DAILY_MANUAL_TESTS = 3;
 
 export const DB_NAME = "testData";
--- packages/qa-pup-core/src/email.ts
+++ packages/qa-pup-core/src/email.ts
@@ -17,47 +17,53 @@ const testCompletionEmailTemplate = `
     </mj-section>
     <mj-section background-color="#FFFFFF">
       <mj-column>
+        <mj-image width="75px" src="https://raw.githubusercontent.com/cloudydaiyz/qa-pup/refs/heads/develop/assets/logo.svg"></mj-image>
         <mj-text font-size="30px" align="center" color="#FF6600"><strong>QA Pup</strong></mj-text>
         <mj-divider border-color="#FF6600"></mj-divider>
         
         <mj-image width="200px" src="https://raw.githubusercontent.com/cloudydaiyz/qa-pup/refs/heads/develop/assets/pups.png"></mj-image>
         <mj-text font-size="28px" align="center" color="#FF6600">The test run has completed</mj-text>
         <mj-button background-color="#FF6600" font-size="18px"><a href="#" style="color: #FFFFFF; text-decoration: none;">View full results at QA Pup</a></mj-button>
+        <mj-text align="center" font-size="18px"><i>The pups miss you!</i></mj-text>
         <mj-text>&nbsp;</mj-text>
         
         <mj-text font-size="20px"><strong>Run ID:</strong> <!-- Run ID --></mj-text>
         <mj-text>&nbsp;</mj-text>
         
         <!-- Run Info -->
+
       </mj-column>
     </mj-section>
     <mj-section>
       <mj-column>
-    		<mj-text align="center" line-height="45px" font-size="13px">Made by Kylan Duncan (@cloudydaiyz)</mj-text>
+    		<mj-text align="center" line-height="0px" font-size="13px">&nbsp;</mj-text>
+    		<mj-text align="center" line-height="0px" font-size="13px">Inspired by <a href="https://qawolf.com">QA Wolf</a></mj-text>
+    		<mj-text align="center" line-height="0px" font-size="13px">Made by Kylan Duncan (<a href="https://github.com/cloudydaiyz">@cloudydaiyz</a>)</mj-text>
+    		<mj-text align="center" line-height="0px" font-size="13px">&nbsp;</mj-text>
       </mj-column>
     </mj-section>
   </mj-body>
 </mjml>
-`
+`;
 
 const testRunBlockTemplate = `
-        <mj-text font-size="20px"><strong><--! name --></strong></mj-text>
-        <mj-text font-size="18px">Duration: <--! duration --></mj-text>
-        <mj-text font-size="18px">Status: <--! status --></mj-text>
+        <mj-text font-size="20px"><strong><--! Name --></strong></mj-text>
+        <mj-text font-size="18px">Duration: <--! Duration --></mj-text>
+        <mj-text font-size="18px">Status: <--! Status --></mj-text>
         <mj-text>&nbsp;</mj-text>
 
-`
+`;
 
 export function composeEmailBody(runId: string, testRuns: TestRunFileSchema[]): string {
     let testRunBlocks = "";
     for(const testRun of testRuns) {
         testRunBlocks += testRunBlockTemplate
-            .replace("<!-- name -->", testRun.name)
-            .replace("<!-- duration -->", testRun.duration.toString())
-            .replace("<!-- status -->", testRun.status);
+            .replace("<!-- Name -->", testRun.name)
+            .replace("<!-- Duration -->", testRun.duration.toString())
+            .replace("<!-- Status -->", testRun.status);
     }
     
-    return testCompletionEmailTemplate
+    return mjml(testCompletionEmailTemplate
         .replace("<!-- Run ID -->", runId)
-        .replace("<!-- Run Info -->", testRunBlocks);
+        .replace("<!-- Run Info -->", testRunBlocks)).html;
 }
