6360c774fe8a7fde84b6b737d7734b04477ee91e
cloudydaiyz <kyland03.biz@gmail.com>
2024-09-16 12:28:13 -0500

    container complete

--- test-container/scripts/run-container.sh
+++ test-container/scripts/run-container.sh
@@ -14,15 +14,15 @@ if [ -f "${DIR}/secrets.sh" ]; then
     export RUN_ID=`node "${DIR}/generate-run.js"`
 fi
 
-# docker run \
-#     -e TEST_INPUT_BUCKET="$TEST_INPUT_BUCKET" \
-#     -e TEST_FILE="$TEST_FILE" \
-#     -e TEST_OUTPUT_BUCKET="$TEST_OUTPUT_BUCKET" \
-#     -e AWS_REGION="$AWS_REGION" \
-#     -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
-#     -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
-#     -e MONGODB_URI="$MONGODB_URI" \
-#     -e MONGODB_USER="$MONGODB_USER" \
-#     -e MONGODB_PASS="$MONGODB_PASS" \
-#     -e RUN_ID="$RUN_ID" \
-#     test-container
+docker run \
+    -e TEST_INPUT_BUCKET="$TEST_INPUT_BUCKET" \
+    -e TEST_FILE="$TEST_FILE" \
+    -e TEST_OUTPUT_BUCKET="$TEST_OUTPUT_BUCKET" \
+    -e AWS_REGION="$AWS_REGION" \
+    -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+    -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+    -e MONGODB_URI="$MONGODB_URI" \
+    -e MONGODB_USER="$MONGODB_USER" \
+    -e MONGODB_PASS="$MONGODB_PASS" \
+    -e RUN_ID="$RUN_ID" \
+    test-container
